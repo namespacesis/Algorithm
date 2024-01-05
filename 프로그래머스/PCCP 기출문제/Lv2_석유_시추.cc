f853d7b348535e5ca6168a1ece0077b4e579046e
namespacesis <usingfullstack@gmail.com>
2024-01-05 14:55:15 +0900

    24.01.05

---
+++ 프로그래머스/PCCP 기출문제/Lv2_석유_시추.cc
@@ -0,0 +1,72 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 501;
+
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+
+bool visited[MAX][MAX];
+
+int maxOil[MAX];
+
+int solution(vector<vector<int>> land) {
+    int answer = 0;
+
+    int N = land.size();
+    int M = land[0].size();
+
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < M; x++) {
+            if (!land[y][x] || visited[y][x]) {
+                continue;
+            }
+
+            set<int> xSet;
+            queue<pair<int, int>> q;
+
+            q.push({ y, x });
+            visited[y][x] = true;
+
+            int cnt = 0;
+
+            while (!q.empty()) {
+                int curY = q.front().first;
+                int curX = q.front().second;
+                xSet.insert(curX);
+                cnt++;
+                q.pop();
+
+                for (int i = 0; i < 4; i++) {
+                    int nextY = curY + dx[i];
+                    int nextX = curX + dy[i];
+
+                    if (nextY < 0 || nextY >= N || nextX < 0 || nextX >= M) {
+                        continue;
+                    }
+
+                    if (visited[nextY][nextX] || !land[nextY][nextX]) {
+                        continue;
+                    }
+
+                    visited[nextY][nextX] = true;
+                    q.push({ nextY, nextX });
+                }
+            }
+
+            for (int x : xSet) {
+                maxOil[x] += cnt;
+            }
+        }
+    }
+
+    sort(maxOil, maxOil + MAX);
+    answer = maxOil[MAX - 1];
+
+    return answer;
+}
