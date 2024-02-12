35cc11a4f7106c2b0ea1fa6af2cf35f58dc6d782
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-02-12 15:44:30 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 484 ms, Memory: 23336 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>0;j--) System.out.print(" ");
+            for(int j=0;j<i;j++) System.out.print("*");
+            System.out.println();
+        }
+        
+        for(int i=n-1;i>=1;i--){
+            for(int j=n-i;j>0;j--) System.out.print(" ");
+            for(int j=0;j<i;j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
