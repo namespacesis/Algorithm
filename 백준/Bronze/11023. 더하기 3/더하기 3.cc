55ddd266db56bbc6c0778900bf5689d6881df13d
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-05-13 09:06:41 +0900

    [Bronze III] Title: 더하기 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	int res = 0;
+
+	while (cin >> n) {
+		res += n;
+	}
+
+	cout << res << "\n";
+	return 0;
+}
