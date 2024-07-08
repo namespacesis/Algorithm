91b4402b99a9f3a958510d934b1b550ae686e6fe
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-07-08 15:55:05 +0900

    [Gold II] Title: 색종이 붙이기, Time: 12 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
@@ -1,13 +1,10 @@
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <limits.h>
 
 int arr[10][10];
-int paper[6];
-int res = 1e9;
+int paper[6] = { 0, 0, 0, 0, 0, 0 };
+int res = INT_MAX;
 
 int check(int x, int y, int k) {
     for (int i = 0; i < k; i++) {
@@ -17,73 +14,63 @@ int check(int x, int y, int k) {
             }
         }
     }
-
     return 1;
 }
 
 void change(int x, int y, int k, int val) {
-	for (int i = 0; i < k; i++) {
-		for (int j = 0; j < k; j++) {
-			arr[x + i][y + j] = val;
-		}
-	}
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            arr[x + i][y + j] = val;
+        }
+    }
 }
 
 void dfs(int x, int y, int cnt) {
-	while (!arr[x][y]) {
-		if (++y == 10) {
-			x++;
-			y = 0;
-		}
+    if (cnt >= res) return;
 
-		if (x == 10) {
-			res = min(res, cnt);
-			return;
-		}
-	}
+    if (x == 10) {
+        if (cnt < res) res = cnt;
+        return;
+    }
 
-	if (res <= cnt) {
-		return;
-	}
+    if (y == 10) {
+        dfs(x + 1, 0, cnt);
+        return;
+    }
 
-	for (int k = 5; k >= 1; k--) {
-		if (paper[k] == 5) {
-			continue;
-		}
+    if (arr[x][y] == 0) {
+        dfs(x, y + 1, cnt);
+        return;
+    }
 
-		if (x + k > 10 || y + k > 10) {
-			continue;
-		}
+    for (int k = 5; k >= 1; k--) {
+        if (paper[k] == 5 || x + k > 10 || y + k > 10 || !check(x, y, k)) {
+            continue;
+        }
 
-		if (!check(x, y, k)) {
-			continue;
-		}
-
-		change(x, y, k, 0);
-		paper[k]++;
-		dfs(x, y, cnt + 1);
-		change(x, y, k, 1);
-		paper[k]--;
-	}
+        change(x, y, k, 0);
+        paper[k]++;
+        dfs(x, y + k, cnt + 1);
+        change(x, y, k, 1);
+        paper[k]--;
+    }
 }
 
-int main() {
-    FastIO;
+int main(void) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
 
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			cin >> arr[i][j];
-		}
-	}
+    dfs(0, 0, 0);
 
-	dfs(0, 0, 0);
+    if (res == INT_MAX) {
+        printf("-1\n");
+    }
+    else {
+        printf("%d\n", res);
+    }
 
-	if (res == 1e9) {
-		cout << -1 << "\n";
-	}
-	else {
-		cout << res << "\n";
-	}
-
-	return 0;
+    return 0;
 }
