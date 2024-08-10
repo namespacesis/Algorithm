88e6891b3f211b59de74ac3f417fef881c874f08
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-08-11 04:24:11 +0900

    [Bronze I] Title: 인생은 한 방, Time: 4 ms, Memory: 1208 KB -BaekjoonHub

--- 백준/Bronze/25183. 인생은 한 방/인생은 한 방.cc
+++ 백준/Bronze/25183. 인생은 한 방/인생은 한 방.cc
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define max(a, b) a > b ? a : b
 
 int N;
 char str[100000];
@@ -13,19 +12,21 @@ int main(void) {
 	}
 
 	int cnt = 1;
-	int res = 0;
 
 	for (int i = 1; i < N; i++) {
 		if (str[i] == str[i - 1] + 1 || str[i] == str[i - 1] - 1) {
 			cnt++;
-			res = max(res, cnt);
+            if (cnt == 5) {
+                printf("YES");
+                return 0;
+            }
 		}
 		else {
 			cnt = 1;
 		}
 	}
 
-	res >= 5 ? printf("YES") : printf("NO");
+	printf("NO");
 
 	return 0;
 }
