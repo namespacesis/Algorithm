083ef05e71163548ed1fe0b54f4fb66122605513
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-08-21 15:02:14 +0900

    [Gold III] Title: 울타리, Time: 168 ms, Memory: 11116 KB -BaekjoonHub

---
+++ 백준/Gold/9347. 울타리/울타리.cc
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <queue>
+
+#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define INF 1e9
+
+using namespace std;
+
+struct Node {
+    int x;
+    int y;
+    int cnt;
+
+    bool operator<(const Node& t) const {
+        return cnt > t.cnt;
+    }
+};
+
+int T;
+int R, C;
+int arr[1000][1000];
+int visited[1000][1000];
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+
+int main() {
+    FastIO;
+
+    cin >> T;
+
+    while (T--) {
+        cin >> R >> C;
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                cin >> arr[i][j];
+                visited[i][j] = INF;
+            }
+        }
+
+        priority_queue<Node> pq;
+
+        for (int i = 0; i < R; i++) {
+            pq.push({ i, 0, arr[i][0] });
+            visited[i][0] = arr[i][0];
+            pq.push({ i, C - 1, arr[i][C - 1] });
+            visited[i][C - 1] = arr[i][C - 1];
+        }
+
+        for (int j = 0; j < C; j++) {
+            pq.push({ 0, j, arr[0][j] });
+			visited[0][j] = arr[0][j];
+			pq.push({ R - 1, j, arr[R - 1][j] });
+			visited[R - 1][j] = arr[R - 1][j];
+        }
+
+        while (!pq.empty()) {
+            Node cur = pq.top();
+            pq.pop();
+
+            if (cur.cnt > visited[cur.x][cur.y]) {
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if (0 <= nx && nx < R && 0 <= ny && ny < C) {
+                    if (arr[nx][ny] && visited[nx][ny] > cur.cnt + 1) {
+                        visited[nx][ny] = cur.cnt + 1;
+                        pq.push({ nx, ny, cur.cnt + 1 });
+                    }
+                    else if (!arr[nx][ny] && visited[nx][ny] > cur.cnt) {
+                        visited[nx][ny] = cur.cnt;
+                        pq.push({ nx, ny, cur.cnt });
+                    }
+                }
+            }
+        }
+
+        int res1 = 0;
+        int res2 = 0;
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (!arr[i][j]) {
+                    if (res1 < visited[i][j]) {
+                        res1 = visited[i][j];
+                        res2 = 1;
+                    }
+                    else if (res1 == visited[i][j]) {
+						res2++;
+					}
+                }
+            }
+        }
+
+        cout << res1 << " " << res2 << "\n";
+    }
+
+    return 0;
+}
