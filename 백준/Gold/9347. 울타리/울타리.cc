98ca32fff93ccaa83f9ea78d944a855b751cc936
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-08-21 15:41:20 +0900

    [Gold III] Title: 울타리, Time: 76 ms, Memory: 7100 KB -BaekjoonHub

--- 백준/Gold/9347. 울타리/울타리.cc
+++ 백준/Gold/9347. 울타리/울타리.cc
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <bitset>
 #include <deque>
 
 #define FastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #define INF 1e9
+#define MAX_SIZE 1000
 
 using namespace std;
 
@@ -10,12 +12,16 @@ struct Node {
     int x, y, cnt;
 };
 
-int T;
-int R, C;
-int arr[1000][1000];
-int visited[1000][1000];
+int T, R, C;
+bitset<MAX_SIZE* MAX_SIZE> visited;
+bitset<MAX_SIZE* MAX_SIZE> arr;
+int cost[MAX_SIZE * MAX_SIZE];
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
+
+inline int getIndex(int x, int y) {
+    return y * C + x;
+}
 
 int main() {
     FastIO;
@@ -24,28 +30,38 @@ int main() {
 
     while (T--) {
         cin >> R >> C;
+        visited.reset();
+        arr.reset();
 
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                cin >> arr[i][j];
-                visited[i][j] = INF;
+                char input;
+                cin >> input;
+                if (input == '1') {
+                    arr.set(getIndex(j, i));
+                }
+                cost[getIndex(j, i)] = INF;
             }
         }
 
         deque<Node> dq;
 
         for (int i = 0; i < R; i++) {
-            dq.push_front({ i, 0, arr[i][0] });
-            visited[i][0] = arr[i][0];
-            dq.push_front({ i, C - 1, arr[i][C - 1] });
-            visited[i][C - 1] = arr[i][C - 1];
+            dq.push_front({ 0, i, arr[getIndex(0, i)] });
+            visited.set(getIndex(0, i));
+            cost[getIndex(0, i)] = arr[getIndex(0, i)];
+            dq.push_front({ C - 1, i, arr[getIndex(C - 1, i)] });
+            visited.set(getIndex(C - 1, i));
+            cost[getIndex(C - 1, i)] = arr[getIndex(C - 1, i)];
         }
 
         for (int j = 1; j < C - 1; j++) {
-            dq.push_front({ 0, j, arr[0][j] });
-            visited[0][j] = arr[0][j];
-            dq.push_front({ R - 1, j, arr[R - 1][j] });
-            visited[R - 1][j] = arr[R - 1][j];
+            dq.push_front({ j, 0, arr[getIndex(j, 0)] });
+            visited.set(getIndex(j, 0));
+            cost[getIndex(j, 0)] = arr[getIndex(j, 0)];
+            dq.push_front({ j, R - 1, arr[getIndex(j, R - 1)] });
+            visited.set(getIndex(j, R - 1));
+            cost[getIndex(j, R - 1)] = arr[getIndex(j, R - 1)];
         }
 
         while (!dq.empty()) {
@@ -56,11 +72,13 @@ int main() {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
 
-                if (0 <= nx && nx < R && 0 <= ny && ny < C) {
-                    int ncnt = cur.cnt + arr[nx][ny];
-                    if (ncnt < visited[nx][ny]) {
-                        visited[nx][ny] = ncnt;
-                        if (arr[nx][ny] == 0) {
+                if (nx >= 0 && nx < C && ny >= 0 && ny < R) {
+                    int index = getIndex(nx, ny);
+                    int ncnt = cur.cnt + arr.test(index);
+                    if (!visited.test(index) || ncnt < cost[index]) {
+                        visited.set(index);
+                        cost[index] = ncnt;
+                        if (arr.test(index) == 0) {
                             dq.push_front({ nx, ny, ncnt });
                         }
                         else {
@@ -76,12 +94,13 @@ int main() {
 
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                if (!arr[i][j]) {
-                    if (res1 < visited[i][j]) {
-                        res1 = visited[i][j];
+                int index = getIndex(j, i);
+                if (!arr.test(index)) {
+                    if (cost[index] > res1) {
+                        res1 = cost[index];
                         res2 = 1;
                     }
-                    else if (res1 == visited[i][j]) {
+                    else if (cost[index] == res1) {
                         res2++;
                     }
                 }
