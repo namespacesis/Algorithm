f11efc70d53a0de08c2e62691bfcbd32efb67eb9
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-10-05 01:57:39 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 428.87 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
@@ -1,61 +1,46 @@
-#include <string>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
 
-unordered_map<int, pair<int, int>> am;
-unordered_map<int, int> um;
+unordered_map<int, pair<int, int>> pointMap;
+unordered_map<int, unordered_map<int, int>> grid;
 int res = 0;
 
-int makeHash(pair<int, int> p, int t) {
-    return p.first * 10000000 + p.second * 100000 + t;
-}
-
-void simulate(vector<int> &route) {
+void simulate(vector<int>& route) {
+    pair<int, int> curPos = pointMap[route[0]];
     int t = 0;
-    pair<int, int> end;
     
-    for (int i = 0; i < route.size() - 1; i++) {
-        pair<int, int> start = am[route[i]];
-        pair<int, int> next = am[route[i + 1]];
-        end = next;
+    for (int i = 1; i < route.size(); i++) {
+        pair<int, int> nextPos = pointMap[route[i]];
         
-        while (start != next) {
-            int bh = makeHash(start, t);
-            um[bh]++;
-            if (um[bh] == 2) res++;
+        while (curPos != nextPos) {
+            grid[t][curPos.first * 1000 + curPos.second]++;
+            if (grid[t][curPos.first * 1000 + curPos.second] == 2) res++;
             
-            int diffX = start.first - next.first;
-            int diffY = start.second - next.second;
-            
-            if (diffX != 0) {
-                diffX > 0 ? start.first-- : start.first++;
-            }
-            else if (diffY != 0) {
-                diffY > 0 ? start.second-- : start.second++;
-            }
+            if (curPos.first != nextPos.first)
+                curPos.first += (curPos.first < nextPos.first) ? 1 : -1;
+            else
+                curPos.second += (curPos.second < nextPos.second) ? 1 : -1;
             
             t++;
         }
     }
-    int eh = makeHash(end, t);
-    um[eh]++;
-    if (um[eh] == 2) res++;
+    grid[t][curPos.first * 1000 + curPos.second]++;
+    if (grid[t][curPos.first * 1000 + curPos.second] == 2) res++;
 }
 
 int solution(vector<vector<int>> points, vector<vector<int>> routes) {
-    int answer = 0;
+    int n = points.size();
+    int x = routes.size();
     
-    for (int i = 1; i <= points.size(); i++) {
-        am[i] = {points[i - 1][0], points[i - 1][1]};
+    for (int i = 0; i < n; i++) {
+        pointMap[i + 1] = {points[i][0], points[i][1]};
     }
     
-    for (auto route : routes) {
+    for (auto& route : routes) {
         simulate(route);
     }
     
-    answer = res;
-    
-    return answer;
+    return res;
 }
