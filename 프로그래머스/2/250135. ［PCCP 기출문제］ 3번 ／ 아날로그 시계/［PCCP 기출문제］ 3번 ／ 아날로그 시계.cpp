9ca9139e4be12a5c753b2d3067fd4e49e6658945
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-10-06 18:55:22 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 0.28 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.cpp
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.cpp
@@ -1,73 +1,62 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <cmath>
-#include <algorithm>
 
 using namespace std;
 
 int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
-    const double epsilon = 1e-7;
-    double omega_s = 6.0; // 초침 각속도 (deg/s)
-    double omega_m = 0.1; // 분침 각속도 (deg/s)
-    double omega_h = 0.5 / 60.0; // 시침 각속도 (deg/s)
-
-    // 시작 시간부터의 총 초
-    double total_time = (h2 * 3600 + m2 * 60 + s2) - (h1 * 3600 + m1 * 60 + s1);
-
-    // 각 바늘의 초기 각도 계산
-    double theta_s = s1 * 6.0;
-    double theta_m = m1 * 6.0 + s1 * 0.1;
-    double theta_h = (h1 % 12) * 30.0 + m1 * 0.5 + s1 * (0.5 / 60.0);
-
-    // 상대 각속도
-    double omega_sm = omega_s - omega_m;
-    double omega_sh = omega_s - omega_h;
-
-    // 각도 차이
-    double deltaTheta_sm = fmod((theta_m - theta_s + 360.0), 360.0);
-    double deltaTheta_sh = fmod((theta_h - theta_s + 360.0), 360.0);
-
-    // 겹치는 시간 계산
-    double t0_sm = deltaTheta_sm / omega_sm;
-    double t0_sh = deltaTheta_sh / omega_sh;
-
-    // 겹침 주기
-    double P_sm = 360.0 / omega_sm;
-    double P_sh = 360.0 / omega_sh;
-
-    vector<double> times;
-
-    // 초침과 분침의 겹치는 시간 추가
-    for (int n = 0;; ++n) {
-        double t = t0_sm + n * P_sm;
-        if (t > total_time) break;
-        if (t >= 0 && t <= total_time) {
-            times.push_back(t);
-        }
-    }
-
-    // 초침과 시침의 겹치는 시간 추가
-    for (int n = 0;; ++n) {
-        double t = t0_sh + n * P_sh;
-        if (t > total_time) break;
-        if (t >= 0 && t <= total_time) {
-            times.push_back(t);
-        }
-    }
-
-    // 시간 정렬 및 중복 제거
-    sort(times.begin(), times.end());
-
     int answer = 0;
-    double prev_time = -1.0;
+    // 시작과 끝 시간을 초 단위로 변환
+    int start = h1 * 3600 + m1 * 60 + s1;
+    int end = h2 * 3600 + m2 * 60 + s2;
 
-    for (double t : times) {
-        if (prev_time < 0 || fabs(t - prev_time) > epsilon) {
-            answer++;
-            prev_time = t;
-        }
+    // 총 시간 계산
+    int totalTime = end - start;
+
+    // 초기 각도 계산 (초침, 분침, 시침)
+    int tempH = (h1 % 12) * (720 * 5) + (m1 * 60 + s1) * 1;
+    int tempM = (m1 * 60 + s1) * 12;
+    int tempS = s1 * 720;
+    int prevS, prevM, prevH;
+
+    // 초기 시점에서 초침, 분침, 시침이 겹치는지 확인
+    if (tempS == tempM || tempS == tempH)
+        ++answer;
+
+    // 시간이 지날 때마다 확인
+    while (totalTime--) {
+        prevS = tempS;
+        prevM = tempM;
+        prevH = tempH;
+
+        // 각도를 1초에 맞게 증가
+        tempH += 1;
+        tempM += 12;
+        tempS += 720;
+
+        // 초침이 분침과 겹치는지 확인
+        if (tempS == tempM)
+            ++answer;
+        else if ((prevS < prevM) != (tempS < tempM)) // 중간에서 겹칠 때
+            ++answer;
+
+        // 초침이 시침과 겹치는지 확인
+        if (tempS == tempH)
+            ++answer;
+        else if ((prevS < prevH) != (tempS < tempH)) // 중간에서 겹칠 때
+            ++answer;
+
+        // 43200(한 바퀴) 도달 시 각도를 0으로 리셋
+        if (tempS == 43200)
+            tempS = 0;
+        if (tempM == 43200)
+            tempM = 0;
+        if (tempH == 43200)
+            tempH = 0;
     }
+
+    // 12시를 넘길 때 1회 알람 감소
+    if (h1 < 12 && h2 >= 12)
+        --answer;
 
     return answer;
 }
