cfd386dfef0b92218ab25f2353d6c7e2d065ee17
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-10-08 10:02:02 +0900

    [level 2] Title: 큰 수 만들기, Time: 371.92 ms, Memory: 5.57 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,37 +1,24 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    
-    int cnt = 0;
-    int idx = 0;
-    int maxV = number[idx] - '0';
-
-    while (cnt < k) {
-        int e = k - cnt + idx;
-        for (int i = idx + 1; i <= e; i++) {
-            if ((number[i] - '0') > maxV) {
-                maxV = number[i] - '0';
-                cnt += i - idx;
-                idx = i;
-                if (cnt == k) {
-                    break;
-                }
+    answer = number.substr(k); 
+    for(int i = k-1;i >=0;i--){
+        int j = 0;
+        do{
+            if(number[i] >= answer[j]){
+                char temp = answer[j];
+                answer[j] = number[i];
+                number[i] = temp;
+                j++;
+            }else{
+                break;
             }
-        }
-        answer += number[idx];
-        if (answer.size() == number.size() - k) break;
-        maxV = number[idx + 1] - '0';
-        idx++;
+        }while(1);
     }
-        
-    if (answer.size() < number.size() - k) {
-        answer += number.substr(idx, number.size());
-    }
-    
+
     return answer;
 }
