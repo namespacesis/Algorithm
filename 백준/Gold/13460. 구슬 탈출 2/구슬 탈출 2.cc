0c98bf220d903e4c87fa3c4e410611d44dda0341
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-10-20 16:37:34 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
@@ -1,92 +1,139 @@
 #include <iostream>
 #include <queue>
-#include <tuple>
+
+#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr);
 
 using namespace std;
 
+struct Node {
+    int br, bc;
+    int rr, rc;
+    int cnt;
+    int dir;
+};
+
 int N, M;
 char arr[10][10];
-int visited[10][10][10][10];
+int bx, by;
+int rx, ry;
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
-int rx, ry, bx, by;
-
-void move(int& x, int& y, int& dis, int i) {
-	while (arr[x + dx[i]][y + dy[i]] != '#' && arr[x][y] != 'O') {
-		x += dx[i];
-		y += dy[i];
-		dis += 1;
-	}
-}
+bool visited[10][10][10][10];
+int res = 11;
 
 int bfs() {
-	queue<tuple<int, int, int, int, int>> q;
-	q.push({ rx, ry, bx, by, 0 });
-	visited[rx][ry][bx][by] = 1;
+    queue<Node> q;
+    for (int i = 0; i < 4; i++) {
+        q.push({ bx, by, rx, ry, 0, i });
+    }
+    visited[bx][by][rx][ry] = true;
 
-	while (!q.empty()) {
-		int crx, cry, cbx, cby, cnt;
-		tie(crx, cry, cbx, cby, cnt) = q.front();
-		q.pop();
+    while (!q.empty()) {
+        Node cur = q.front();
+        q.pop();
+        
+        if (cur.cnt == 10) continue;
 
-		if (cnt >= 10) return -1;
+        int nrx = cur.rr;
+        int nry = cur.rc;
+        int nbx = cur.br;
+        int nby = cur.bc;
+        bool rflag = false;
+        bool bflag = false;
 
-		for (int i = 0; i < 4; i++) {
-			int nrx = crx, nry = cry, nbx = cbx, nby = cby;
-			int rdis = 0, bdis = 0;
+        while (true) {
+            if (!rflag) {
+                nrx += dx[cur.dir];
+                nry += dy[cur.dir];
+            }
+            if (!bflag) {
+                nbx += dx[cur.dir];
+                nby += dy[cur.dir];
+            }
 
-			move(nrx, nry, rdis, i);
-			move(nbx, nby, bdis, i);
+            if (!rflag && arr[nrx][nry] == '#') {
+                nrx -= dx[cur.dir];
+				nry -= dy[cur.dir];
+                rflag = true;
+            }
+            if (!bflag && arr[nbx][nby] == '#') {
+                nbx -= dx[cur.dir];
+                nby -= dy[cur.dir];
+                bflag = true;
+            }
 
-			if (arr[nbx][nby] == 'O') continue;
-			if (arr[nrx][nry] == 'O') {
-				return cnt + 1;
+            if (!rflag && arr[nrx][nry] == 'O') {
+                nrx = -1;
+				nry = -1;
+                rflag = true;
+            }
+            if (!bflag && arr[nbx][nby] == 'O') {
+				nbx = -1;
+				nby = -1;
+                bflag = true;
 			}
 
-			if (nrx == nbx && nry == nby) {
-				if (rdis > bdis) {
-					nrx -= dx[i];
-					nry -= dy[i];
+            if (rflag && bflag) break;
+
+            if (nrx == nbx && nry == nby) {
+                if (rflag) {
+					nbx -= dx[cur.dir];
+					nby -= dy[cur.dir];
+                    bflag = true;
+                    break;
 				}
 				else {
-					nbx -= dx[i];
-					nby -= dy[i];
+					nrx -= dx[cur.dir];
+					nry -= dy[cur.dir];
+                    rflag = true;
+                    break;
 				}
-			}
+            }
+        }
 
-			if (!visited[nrx][nry][nbx][nby]) {
-				visited[nrx][nry][nbx][nby] = 1;
-				q.push({ nrx, nry, nbx, nby, cnt + 1 });
-			}
-		}
-	}
-	return -1;
+        if (nrx == -1 && nbx != -1) {
+            res = min(res, cur.cnt + 1);
+            continue;
+        }
+        else if (nbx == -1) continue;
+
+        for (int i = 0; i < 4; i++) {
+            if (cur.dir == i) continue;
+            if (!visited[nbx][nby][nrx][nry]) {
+                q.push({ nbx, nby, nrx, nry, cur.cnt + 1, i });
+            }
+        }
+        visited[nbx][nby][nrx][nry] = true;
+    }
+
+    if (res == 11) return -1;
+    return res;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    FastIO;
+    
+    cin >> N >> M;
 
-	cin >> N >> M;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
-			if (arr[i][j] == 'R') {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 'B') {
+                bx = i;
+                by = j;
+                arr[i][j] = '.';
+            }
+            if (arr[i][j] == 'R') {
 				rx = i;
 				ry = j;
+                arr[i][j] = '.';
 			}
-			else if (arr[i][j] == 'B') {
-				bx = i;
-				by = j;
-			}
-		}
-	}
+        }
+    }
 
-	int res;
-	res = bfs();
+    int res = bfs();
 
-	cout << res << "\n";
-	return 0;
+    cout << res << "\n";
+
+    return 0;
 }
