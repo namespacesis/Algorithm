89fba25d87a4908be0bb9c4792acc563965c69e9
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-11-20 16:54:26 +0900

    [Gold IV] Title: 암벽 등반, Time: 80 ms, Memory: 11108 KB -BaekjoonHub

---
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <unordered_map>
+#include <queue>
+
+#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr);
+#define ll long long
+using namespace std;
+
+struct Node {
+    int x, y;
+    int cnt;
+};
+
+int N, T;
+unordered_map<ll, bool> um;
+unordered_map<ll, bool> visited;
+
+int main() {
+    FastIO;
+
+    cin >> N >> T;
+
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        ll hash = (ll)a * 1000000 + b;
+        um[hash] = true;
+    }
+
+    queue<Node> q;
+    q.push({0, 0, 0});
+    visited[0] = false;
+
+    while (!q.empty()) {
+        Node cur = q.front();
+        q.pop();
+
+        if (cur.y == T) {
+            cout << cur.cnt << "\n";
+            return 0;
+        }
+
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2; j <= 2; j++) {
+                if (i == 0 && j == 0) continue;
+
+                int nx = cur.x + i;
+                int ny = cur.y + j;
+
+                if (nx < 0 || nx > 1000000 || ny < 0 || ny > T) continue;
+
+                ll hash = (ll)nx * 1000000 + ny;
+
+                if (um[hash] && !visited[hash]) {
+                    visited[hash] = true;
+                    q.push({ nx, ny, cur.cnt + 1 });
+                }
+            }
+        }
+    }
+
+    cout << -1 << "\n";
+
+    return 0;
+}
