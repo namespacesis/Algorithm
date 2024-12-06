8f48ada865371e32817e31fac0316cf47e4b2b0e
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-12-06 20:56:04 +0900

    [Gold V] Title: 행복 유치원, Time: 52 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr);
+#define endl '\n'
+
+using namespace std;
+
+int N, K;
+
+int main() {
+    FastIO;
+
+    cin >> N >> K;
+
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> diff(N - 1);
+
+    for (int i = 0; i < N - 1; i++) {
+        diff[i] = arr[i + 1] - arr[i];
+    }
+
+    sort(diff.begin(), diff.end());
+
+    int res = 0;
+
+    for (int i = 0; i < N - K; i++) {
+        res += diff[i];
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
