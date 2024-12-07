23b749e6ccd584a5c284d42f52fd614e4f0d2de3
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-12-07 16:59:14 +0900

    [Gold V] Title: 근성아 일하자, Time: 52 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/32358. 근성아 일하자/근성아 일하자.cc
+++ 백준/Gold/32358. 근성아 일하자/근성아 일하자.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <map>
-#include <cmath>
+#include <vector>
+#include <algorithm>
 
 #define FastIO ios::sync_with_stdio(false), cin.tie(nullptr);
 #define endl '\n'
@@ -8,16 +8,50 @@
 using namespace std;
 
 int N;
-map<int, int> left_trees;
-map<int, int> right_trees;
+vector<int> arrP;
+vector<int> arrM;
+int idx = 0;
+long long res = 0;
+
+void solve() {
+    sort(arrM.begin(), arrM.end());
+    sort(arrP.begin(), arrP.end(), greater<int>());
+
+    while (arrP.size() && arrM.size()) {
+        int p = arrP.back() - idx;
+        int m = idx - arrM.back();
+
+        if (m <= p) {
+            idx = arrM.back();
+            res += m;
+            arrM.pop_back();
+        }
+        else {
+            idx = arrP.back();
+            res += p;
+            arrP.pop_back();
+        }
+    }
+
+    while (arrP.size()) {
+        int p = arrP.back() - idx;
+        idx = arrP.back();
+        res += p;
+        arrP.pop_back();
+    }
+
+    while (arrM.size()) {
+        int m = idx - arrM.back();
+        idx = arrM.back();
+        res += m;
+        arrM.pop_back();
+    }
+}
 
 int main() {
     FastIO;
 
     cin >> N;
-
-    int idx = 0;
-    long long res = 0;
 
     for (int i = 0; i < N; i++) {
         int q;
@@ -27,40 +61,15 @@ int main() {
             int n;
             cin >> n;
 
-            if (n < idx) {
-                left_trees[-n]++;
+            if (n >= idx) {
+                arrP.push_back(n);
             }
             else {
-                right_trees[n]++;
+                arrM.push_back(n);
             }
         }
-        else if (q == 2) {
-            while (!left_trees.empty() || !right_trees.empty()) {
-                int left_distance = left_trees.empty() ? 1e9 : abs(-left_trees.begin()->first - idx);
-
-                int right_distance = right_trees.empty() ? 1e9 : abs(right_trees.begin()->first - idx);
-
-                if (left_distance <= right_distance) {
-                    auto it = left_trees.begin();
-                    int pos = -it->first;
-                    res += abs(pos - idx);
-                    idx = pos;
-
-                    if (--it->second == 0) {
-                        left_trees.erase(it);
-                    }
-                }
-                else {
-                    auto it = right_trees.begin();
-                    int pos = it->first;
-                    res += abs(pos - idx);
-                    idx = pos;
-
-                    if (--it->second == 0) {
-                        right_trees.erase(it);
-                    }
-                }
-            }
+        else {
+            solve();
         }
     }
 
