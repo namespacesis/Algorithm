d31e7b71516f84c6dca840f7b7265a79c5248fb1
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2024-12-29 16:36:01 +0900

    [Gold II] Title: 나연 정렬, Time: 88 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Gold/32873. 나연 정렬/나연 정렬.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr);
+#define endl '\n'
+#define ll long long
+
+using namespace std;
+
+int N;
+
+int main() {
+    FastIO;
+
+    cin >> N;
+
+    vector<int> v;
+    
+    int a;
+    cin >> a;
+    v.push_back(a);
+
+    for (int i = 1; i < N; i++) {
+        cin >> a;
+
+        if (v.back() < a) {
+            v.push_back(a);
+        }
+        else {
+            auto it = lower_bound(v.begin(), v.end(), a);
+            *it = a;
+        }
+    }
+
+    int res = v.size();
+
+    cout << res << endl;
+
+    return 0;
+}
