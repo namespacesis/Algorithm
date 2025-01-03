2ac9921de3251a1a4ef57316ee559bcda5fe5153
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2025-01-03 17:32:45 +0900

    [Gold III] Title: 보스몬스터 전리품, Time: 24 ms, Memory: 4108 KB -BaekjoonHub

--- 백준/Gold/20005. 보스몬스터 전리품/보스몬스터 전리품.cc
+++ 백준/Gold/20005. 보스몬스터 전리품/보스몬스터 전리품.cc
@@ -1,27 +1,55 @@
 #include <iostream>
 #include <queue>
 
-#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr);
+#define FastIO ios::sync_with_stdio(false), cin.tie(nullptr)
 #define endl '\n'
 #define ll long long
 
 using namespace std;
 
-struct Player {
+struct Node {
     int x, y;
-    int idx;
+    int cnt;
 };
 
 int M, N, P;
 int HP;
 char arr[1000][1000];
-bool visited[1000][1000][26];
-int dps[26];
+bool visited[1000][1000];
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
-queue<Player> qList[26];
-int ar, ac;
-bool check[26];
+int dps[26];
+int sr, sc;
+queue<pair<int, char>> player;
+
+void bfs() {
+    queue<Node> q;
+    q.push({ sr, sc, 0 });
+    visited[sr][sc] = true;
+
+    while (!q.empty()) {
+        Node cur = q.front();
+        q.pop();
+
+        if ('a' <= arr[cur.x][cur.y] && arr[cur.x][cur.y] <= 'z') {
+            player.push({ cur.cnt, arr[cur.x][cur.y] });
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.x + dx[i];
+            int ny = cur.y + dy[i];
+
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
+
+            if (visited[nx][ny]) continue;
+
+            if (arr[nx][ny] == 'X') continue;
+
+            visited[nx][ny] = true;
+            q.push({ nx, ny, cur.cnt + 1 });
+        }
+    }
+}
 
 int main() {
     FastIO;
@@ -31,56 +59,43 @@ int main() {
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             cin >> arr[i][j];
-            if (arr[i][j] >= 'a' && arr[i][j] <= 'z') {
-				qList[arr[i][j] - 'a'].push({i, j, arr[i][j] - 'a'});
-                arr[i][j] = '.';
-			}
             if (arr[i][j] == 'B') {
-                ar = i;
-                ac = j;
+                sr = i;
+                sc = j;
             }
         }
     }
 
     for (int i = 0; i < P; i++) {
         char c;
-        cin >> c;
-        cin >> dps[c - 'a'];
-	}
+        int d;
+        cin >> c >> d;
+        dps[c - 'a'] = d;
+    }
 
     cin >> HP;
 
-    while (HP > 0) {
-        for (int i = 0; i < P; i++) {
-            if (visited[ar][ac][i]) {
-                check[i] = true;
-                HP -= dps[i];
-                continue;
-            }
-            int size = qList[i].size();
-            for (int j = 0; j < size; j++) {
-				Player cur = qList[i].front();
-				qList[i].pop();
-
-				for (int k = 0; k < 4; k++) {
-					int nx = cur.x + dx[k];
-					int ny = cur.y + dy[k];
-
-					if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
-					if (visited[nx][ny][i]) continue;
-					if (arr[nx][ny] == 'X') continue;
-
-					visited[nx][ny][i] = true;
-					qList[i].push({nx, ny, i});
-				}
-			}
-        }
-    }
+    bfs();
 
     int res = 0;
 
-    for (int i = 0; i < P; i++) {
-        if (check[i]) res++;
+    int dmg = 0;
+    int time = 0;
+
+    while (HP > 0) {
+        time++;
+        while (!player.empty()) {
+            int cnt = player.front().first;
+            char c = player.front().second;
+            int d = dps[c - 'a'];
+            if (cnt > time) {
+                break;
+            }
+            player.pop();
+            dmg += d;
+            res++;
+        }
+        HP -= dmg;
     }
 
     cout << res << endl;
