e1a12b1e99e1e87a13a886b1ad823a11e6b879fc
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2025-01-06 19:28:14 +0900

    [Gold I] Title: 전구를 켜라, Time: 12 ms, Memory: 4760 KB -BaekjoonHub

--- 백준/Gold/2423. 전구를 켜라/전구를 켜라.cc
+++ 백준/Gold/2423. 전구를 켜라/전구를 켜라.cc
@@ -1,129 +1,90 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <cstring>
-#include <limits>
+#include <deque>
+#include <climits>
 
 #define FastIO ios::sync_with_stdio(false), cin.tie(nullptr)
 #define endl '\n'
+#define ll long long
+
 using namespace std;
 
-static const int INF = numeric_limits<int>::max();
-
-// 우선순위 큐에 넣을 노드 정보
-//  - x, y : 모서리(격자선 교차점)의 좌표
-//  - t    : 지금까지 회전 횟수
 struct Node {
-    int x, y, t; // t = 회전횟수
-    Node(int _x, int _y, int _t) : x(_x), y(_y), t(_t) {}
-};
-
-// 우선순위 큐에서 회전 횟수(t)가 작은 순으로 정렬
-struct cmp {
-    bool operator()(const Node &a, const Node &b) {
-        return a.t > b.t;
-    }
+    int x, y;
+    int cnt;
 };
 
 int N, M;
+int arr[501][501];
+int visited[501][501];
+int dx[4] = { -1, 1, 1, -1 };
+int dy[4] = { 1, 1, -1, -1 };
+const int INF = INT_MAX;
 
-// '\' -> 0, '/' -> 1
-int arr[501][501];   // 타일 정보 (N×M)
-int dp[501][501];    // dp[x][y] = (x,y)까지 최소 회전 횟수
-
-// 이동 방향 4가지 (자바 코드와 동일)
-int dx[4] = {-1, +1, +1, -1};
-int dy[4] = {+1, +1, -1, -1};
-
-// needRotate(x, y, d) : (x,y)에서 방향 d로 이동시, 지나가는 타일이 회전이 필요한지 여부
 bool needRotate(int x, int y, int d) {
-    // d==0 -> 위+오른쪽 → 지나가는 타일 = arr[x-1][y]
-    // d==1 -> 아래+오른쪽 -> 지나가는 타일 = arr[x][y]
-    // d==2 -> 아래+왼쪽 -> 지나가는 타일 = arr[x][y-1]
-    // d==3 -> 위+왼쪽 -> 지나가는 타일 = arr[x-1][y-1]
-    if (d == 0) {
-        // x-1 >= 0, y >= 0
-        return arr[x-1][y] == 0; // '\'인지(==0)? => 회전 필요(true) 
-    } else if (d == 1) {
-        // x >= 0, y >= 0
-        return arr[x][y] == 1;   // '/'인지(==1)? => 회전 필요(true)
-    } else if (d == 2) {
-        // x >= 0, y-1 >= 0
-        return arr[x][y-1] == 0; 
-    } else {
-        // d == 3
-        // x-1 >= 0, y-1 >= 0
-        return arr[x-1][y-1] == 1; 
-    }
+    if (d == 0) return arr[x - 1][y] == 0;
+    else if (d == 1) return arr[x][y] == 1;
+    else if (d == 2) return arr[x][y - 1] == 0;
+    else return arr[x - 1][y - 1] == 1;
 }
 
-// 다익스트라(또는 0-1 BFS 유사) 방식으로 최소 회전 횟수 탐색
 void zeroOneBfs() {
-    // dp[x][y] = INF 로 초기화
-    for(int i=0; i<=N; i++){
-        for(int j=0; j<=M; j++){
-            dp[i][j] = INF;
+    deque<Node> dq;
+    dq.push_back({0, 0, 0});
+    visited[0][0] = 0;
+
+    while (!dq.empty()) {
+        Node cur = dq.front();
+        dq.pop_front();
+
+        if (cur.x == N && cur.y == M) {
+            break;
         }
-    }
-    dp[0][0] = 0;
 
-    priority_queue<Node, vector<Node>, cmp> pq;
-    pq.push(Node(0, 0, 0));
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.x + dx[i];
+            int ny = cur.y + dy[i];
 
-    while(!pq.empty()) {
-        Node cur = pq.top();
-        pq.pop();
+            if (nx < 0 || nx > N || ny < 0 || ny > M) continue;
 
-        int x = cur.x;
-        int y = cur.y;
-        int t = cur.t;
+            int check = needRotate(cur.x, cur.y, i) ? 1 : 0;
 
-        // 목표 도착점: (N, M)에 도달하면 멈춰도 됨
-        if(x == N && y == M) break;
+            int ncnt = cur.cnt + check;
 
-        // 4가지 방향으로 이동
-        for(int d=0; d<4; d++){
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            // 범위 체크: (0 <= nx <= N, 0 <= ny <= M)
-            if(nx < 0 || nx > N || ny < 0 || ny > M) continue;
+            if (visited[nx][ny] <= ncnt) continue;
 
-            // 이동에 필요한 회전 증가 여부
-            int cost = needRotate(x, y, d) ? 1 : 0;
-            int nt = t + cost;
-
-            if(dp[nx][ny] > nt){
-                dp[nx][ny] = nt;
-                pq.push(Node(nx, ny, nt));
-            }
+            visited[nx][ny] = ncnt;
+            
+            if (check) dq.push_back({ nx, ny, ncnt });
+            else dq.push_front({ nx, ny, ncnt });
         }
     }
 }
 
-int main(){
+int main() {
     FastIO;
 
     cin >> N >> M;
-    // arr[][]: N행 M열
-    for(int i=0; i<N; i++){
-        string row;
-        cin >> row;
-        for(int j=0; j<M; j++){
-            if(row[j] == '/') arr[i][j] = 1;
-            else arr[i][j] = 0;  // '\' 
+
+    char c;
+
+    for (int i = 0; i < N; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < M; j++) {
+            if (s[j] == '/') arr[i][j] = 1;
+            else arr[i][j] = 0;
+        }
+    }
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= M; j++) {
+            visited[i][j] = INF;
         }
     }
 
-    // 최소 회전 횟수 구하기
     zeroOneBfs();
 
-    // dp[N][M] == INF 이면 도달 불가
-    if(dp[N][M] == INF) {
-        cout << "NO SOLUTION" << endl;
-    } else {
-        cout << dp[N][M] << endl;
-    }
+    if (visited[N][M] == INF) cout << "NO SOLUTION" << endl;
+	else cout << visited[N][M] << endl;
 
     return 0;
 }
