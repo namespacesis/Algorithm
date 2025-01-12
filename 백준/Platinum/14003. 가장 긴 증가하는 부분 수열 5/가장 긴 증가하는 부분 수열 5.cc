ae4e416f2b292ce3485402dc891c9d71b7f401e5
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2025-01-12 18:17:33 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 188 ms, Memory: 42360 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,54 +1,60 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
+    int N;
     cin >> N;
-    vector<int> augment;
-    vector<int> sequence;
-    vector<int> DP(N);
-    for (int i = 0; i < N; i++) {
-        DP[i] = 1;
-        int temp;
-        cin >> temp;
-        augment.push_back(temp);
-        if (sequence.size() == 0 or sequence.back() < temp) {
-            sequence.push_back(temp);
-            DP[i] = sequence.size();
+
+    vector<long long> A(N);
+    for(int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    
+    vector<long long> v;
+    vector<int> idx(N);
+    vector<int> parent(N, -1);
+
+    v.push_back(A[0]);
+    idx[0] = 0;
+    int length = 1;
+
+    for(int i = 1; i < N; i++){
+        if(A[i] > v.back()){
+            parent[i] = idx[length - 1];
+            v.push_back(A[i]);
+            idx[length] = i;
+            length++;
         }
         else {
-            *lower_bound(sequence.begin(), sequence.end(), temp) = temp;
-            DP[i] = lower_bound(sequence.begin(), sequence.end(), temp) - sequence.begin() + 1;
-        }
-    }
-    cout << sequence.size() << "\n";
+            auto it = lower_bound(v.begin(), v.end(), A[i]);
+            int pos = it - v.begin();
 
-    int cnt = sequence.size();
+            *it = A[i];
+            idx[pos] = i;
 
-    vector<int> answer;
-
-    for (int i = N - 1; i >= 0; i--) {
-        if (cnt == 0) {
-            break;
-        }
-        if (DP[i] == cnt) {
-            answer.push_back(augment[i]);
-            cnt--;
+            if(pos > 0){
+                parent[i] = idx[pos - 1];
+            }
         }
     }
 
-    for (int i = answer.size() - 1; i >= 0; i--) {
-        cout << answer[i] << " ";
+    cout << length << "\n";
+    
+    vector<long long> ans;
+    int cur = idx[length - 1];
+    while(cur != -1){
+        ans.push_back(A[cur]);
+        cur = parent[cur];
     }
+    reverse(ans.begin(), ans.end());
 
+    for(auto &x : ans){
+        cout << x << " ";
+    }
     cout << "\n";
+
     return 0;
 }
