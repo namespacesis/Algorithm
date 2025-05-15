94530b311291e2df4843dbdf3a5788f1c9c14a79
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2025-05-15 22:40:21 +0900

    [Silver V] Title: 숫자 게임, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2303. 숫자 게임/숫자 게임.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+score = []
+
+for _ in range(N):
+    card = list(map(int,input().split()))
+    max_digit = 0
+    for i in range(5):
+        for j in range(i+1, 5):
+            for m in range(j+1, 5):
+                digit = (card[i]+card[j]+card[m])%10
+                if digit >= max_digit:
+                    max_digit = digit
+    score.append(max_digit)
+
+for i in range(N-1, -1, -1):
+    if score[i] == max(score):
+        print(i + 1)
+        break
