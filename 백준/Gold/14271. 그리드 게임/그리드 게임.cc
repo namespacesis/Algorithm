6ba0639e7d3d247ec61b5377c40b17b4c0135059
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2025-06-05 23:58:11 +0900

    [Gold V] Title: 그리드 게임, Time: 84 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Gold/14271. 그리드 게임/그리드 게임.cc
+++ 백준/Gold/14271. 그리드 게임/그리드 게임.cc
@@ -6,59 +6,32 @@ using namespace std;
 #define endl '\n'
 
 struct Cell {
-    int x;
-    int y;
-    int cnt;
+    int x, y, cnt;
 };
 
-int N, M, K;
-char arr[50][50];
+const int SIZE = 3100;
+const int OFFSET = 1550;
+bool visited[SIZE][SIZE];
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
-unordered_map<int, bool> visited;
-queue<Cell> q;
-int res = 0;
-
-int getHash(int x, int y) {
-    return x * 100000 + y;
-}
-
-void bfs() {
-    while (!q.empty()) {
-        Cell cur = q.front();
-        q.pop();
-
-        if (cur.cnt == K) continue;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = cur.x + dx[i];
-            int ny = cur.y + dy[i];
-
-            int nHash = getHash(nx, ny);
-
-            if (visited[nHash]) continue;
-
-            visited[nHash] = true;
-            q.push({nx, ny, cur.cnt + 1});
-            res++;
-        }
-    }
-}
 
 int main() {
     FastIO;
+    int N, M, K;
+    cin >> N >> M;
 
-	cin >> N >> M;
-
-    char c;
+    queue<Cell> q;
+    int res = 0;
 
     for (int i = 0; i < N; i++) {
+        string row;
+        cin >> row;
         for (int j = 0; j < M; j++) {
-            cin >> c;
-
-            if (c == 'o') {
-                visited[getHash(i, j)] = true;
-                q.push({i, j, 0});
+            if (row[j] == 'o') {
+                int x = i + OFFSET;
+                int y = j + OFFSET;
+                visited[x][y] = true;
+                q.push({x, y, 0});
                 res++;
             }
         }
@@ -66,9 +39,22 @@ int main() {
 
     cin >> K;
 
-    bfs();
+    while (!q.empty()) {
+        Cell cur = q.front(); q.pop();
+        if (cur.cnt == K) continue;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.x + dx[i];
+            int ny = cur.y + dy[i];
+
+            if (!visited[nx][ny]) {
+                visited[nx][ny] = true;
+                q.push({nx, ny, cur.cnt + 1});
+                res++;
+            }
+        }
+    }
 
     cout << res << endl;
-
     return 0;
 }
