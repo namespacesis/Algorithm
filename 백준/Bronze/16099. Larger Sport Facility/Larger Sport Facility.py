ebf44d711ad422bbb0f0f9539bf40571fdf67863
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2025-08-17 22:14:53 +0900

    [Bronze IV] Title: Larger Sport Facility, Time: 180 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/16099. Larger Sport Facility/Larger Sport Facility.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for _ in range(T):
+    LT, WT, LE, WE = map(int, input().split())
+    if LT * WT > LE * WE :
+        print("TelecomParisTech")
+    elif LT * WT < LE * WE :
+        print("Eurecom")
+    else :
+        print("Tie")
