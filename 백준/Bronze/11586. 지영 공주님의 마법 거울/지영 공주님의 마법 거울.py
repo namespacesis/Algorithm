3652da39215f9a25c72c7b1532946e5d165622c0
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2025-10-16 21:22:34 +0900

    [Bronze III] Title: 지영 공주님의 마법 거울, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11586. 지영 공주님의 마법 거울/지영 공주님의 마법 거울.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+mirror = [input().rstrip() for _ in range(n)]
+k = int(input())
+
+if k == 1:
+    print(*mirror, sep='\n')
+elif k == 2:
+    print(*[i[::-1] for i in mirror], sep='\n')
+else:
+    print(*mirror[::-1], sep='\n')
