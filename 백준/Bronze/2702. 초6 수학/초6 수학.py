115d1b8f9c52a66561d8ac83931a79e86d917943
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2026-01-15 20:39:32 +0900

    [Bronze II] Title: 초6 수학, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2702. 초6 수학/초6 수학.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(x, y):
+    while y:
+        if x > y:
+            x, y = y, x
+        y %= x
+    return x
+
+T = int(input())
+for _ in range(T):
+    a, b = map(int, input().split())
+    gcd_num = gcd(a, b)
+    print(a * b // gcd_num, gcd_num)
