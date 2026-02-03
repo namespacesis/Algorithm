88469dc4134c0293388b40bfa66d86f3b232707b
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2026-02-03 23:06:57 +0900

    [Bronze III] Title: 아이 러브 크로아티아, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9517. 아이 러브 크로아티아/아이 러브 크로아티아.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+n = int(input())
+time = 0
+for _ in range(n):
+    t, z = input().split()
+    t = int(t)
+    time += t
+    if time >= 210:
+        print(k)
+        break
+    if z == 'T':
+        k = (k+1)%9
+        if k == 0:
+            k += 1
