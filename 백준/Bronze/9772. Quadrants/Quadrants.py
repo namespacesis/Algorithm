3aff2d3e7a3445a9b17bba6a4f7d5f617e1443fd
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2026-02-28 23:23:11 +0900

    [Bronze IV] Title: Quadrants, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.py
@@ -0,0 +1,19 @@
+def sol(x, y):
+    if x == 0 or y == 0:
+        return 'AXIS'
+    elif x > 0 and y > 0:
+        return 'Q1'
+    elif x < 0 and y > 0:
+        return 'Q2'
+    elif x < 0 and y < 0:
+        return 'Q3'
+    elif x > 0 and y < 0:
+        return 'Q4'
+ 
+ 
+if __name__ == '__main__':
+    while True:
+        x, y = map(float, input().split())
+        print(sol(x, y))
+        if x == 0 and y == 0:
+            break
