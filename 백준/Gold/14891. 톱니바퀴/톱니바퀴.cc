f84e0167819fb2b47eb32d462c7d98a890b88f86
심상익 (Sim SangIk) <usingfullstack@gmail.com>
2026-04-01 00:44:09 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -11,9 +11,13 @@ int arr[5][8];
 int rot[5];
 vector<pair<int, int>> v;
 
-void rotate(int n, int r) {
+void rotate_gear(int n, int r) {
     rot[n] += r;
     rot[n] %= 8;
+}
+
+int get_val(int n, int idx) {
+    return arr[n][((idx - rot[n]) + 8) % 8];
 }
 
 int main() {
@@ -22,91 +26,47 @@ int main() {
     for (int i = 1; i <= 4; ++i) {
         string s;
         cin >> s;
-
         for (int j = 0; j < 8; ++j) {
             arr[i][j] = s[j] - '0';
         }
     }
 
     cin >> k;
-
-    int n, r;
-
-    for (int i = 0; i < k; ++i) {
-
-        for (auto c : v) {
-            rotate(c.first, c.second);
-        }
-        v.clear();
-
+    while (k--) {
+        int n, r;
         cin >> n >> r;
 
-        if (n == 1) {
-            v.push_back({1, r});
+        v.clear();
+        v.push_back({n, r});
 
-            if (arr[1][((2 - rot[1]) + 8) % 8] == arr[2][((6 - rot[2]) + 8) % 8]) continue;
-            v.push_back({2, -r});
-
-            if (arr[2][((2 - rot[2]) + 8) % 8] == arr[3][((6 - rot[3]) + 8) % 8]) continue;
-            v.push_back({3, r});
-
-            if (arr[3][((2 - rot[3]) + 8) % 8] == arr[4][((6 - rot[4]) + 8) % 8]) continue;
-            v.push_back({4, -r});
+        int curr_r = r;
+        for (int i = n; i > 1; --i) {
+            if (get_val(i, 6) != get_val(i - 1, 2)) {
+                curr_r = -curr_r;
+                v.push_back({i - 1, curr_r});
+            } else break;
         }
-        else if (n == 2) {
-            v.push_back({2, r});
 
-            if (arr[2][((6 - rot[2]) + 8) % 8] != arr[1][((2 - rot[1]) + 8) % 8]) {
-                v.push_back({1, -r});
-            }
-
-            if (arr[2][((2 - rot[2]) + 8) % 8] != arr[3][((6 - rot[3]) + 8) % 8]) {
-                v.push_back({3, -r});
-            }
-            else continue;
-
-            if (arr[3][((2 - rot[3]) + 8) % 8] == arr[4][((6 - rot[4]) + 8) % 8]) continue;
-            v.push_back({4, r});
+        curr_r = r;
+        for (int i = n; i < 4; ++i) {
+            if (get_val(i, 2) != get_val(i + 1, 6)) {
+                curr_r = -curr_r;
+                v.push_back({i + 1, curr_r});
+            } else break;
         }
-        else if (n == 3) {
-            v.push_back({3, r});
-            
-            if (arr[3][((2 - rot[3]) + 8) % 8] != arr[4][((6 - rot[4]) + 8) % 8]) {
-                v.push_back({4, -r});
-            }
 
-            if (arr[3][((6 - rot[3]) + 8) % 8] != arr[2][((2 - rot[2]) + 8) % 8]) {
-                v.push_back({2, -r});
-            }
-            else continue;
-
-            if (arr[2][((6 - rot[2]) + 8) % 8] == arr[1][((2 - rot[1]) + 8) % 8]) continue;
-            v.push_back({1, r});
+        for (auto c : v) {
+            rotate_gear(c.first, c.second);
         }
-        else if (n == 4) {
-            v.push_back({4, r});
-
-            if (arr[3][((2 - rot[3]) + 8) % 8] == arr[4][((6 - rot[4]) + 8) % 8]) continue;
-            v.push_back({3, -r});
-
-            if (arr[2][((2 - rot[2]) + 8) % 8] == arr[3][((6 - rot[3]) + 8) % 8]) continue;
-            v.push_back({2, r});
-
-            if (arr[1][((2 - rot[1]) + 8) % 8] == arr[2][((6 - rot[2]) + 8) % 8]) continue;
-            v.push_back({1, -r});
-        }
-    }
-
-    for (auto c : v) {
-        rotate(c.first, c.second);
     }
 
     int res = 0;
 
-    res += arr[1][((0 - rot[1]) + 8) % 8];
-    res += arr[2][((0 - rot[2]) + 8) % 8] * 2;
-    res += arr[3][((0 - rot[3]) + 8) % 8] * 4;
-    res += arr[4][((0 - rot[4]) + 8) % 8] * 8;
+    for (int i = 1; i <= 4; ++i) {
+        if (get_val(i, 0) == 1) {
+            res += (1 << (i - 1));
+        }
+    }
     
     cout << res << endl;
 
